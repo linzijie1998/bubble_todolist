e95725dde8be958f723f1836ad7255d1ed6eedd3
linzijie <linzijie1998@126.com>
2022-10-27 16:10:29 +0800

    remove gorm.Model

--- dao/mysql.go
+++ dao/mysql.go
@@ -1,6 +1,9 @@
 package dao
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 var (
 	DB *gorm.DB
--- models/todo.go
+++ models/todo.go
@@ -1,12 +1,7 @@
 package models
 
-import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-)
-
 type Todo struct {
-	gorm.Model
+	//gorm.Model
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	Status bool   `json:"status"`
